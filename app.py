5b7f0a2ab78e9a8f359cb86f8e73de14ccded4e8
Carkey-Aakash <carkeyaakash360@gmail.com>
2025-11-29 20:58:54 +0545

    added email/sms code

---
+++ app.py
@@ -0,0 +1,66 @@
+import pickle
+import string
+
+import nltk
+import streamlit as st
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+
+ps = PorterStemmer()
+nltk.download('punkt')
+nltk.download('punkt_tab')
+
+tfidf = pickle.load(open('Vectorizer.pkl', 'rb'))
+model = pickle.load(open('model.pkl', 'rb'))
+
+nltk.download('stopwords')
+
+
+def transform_text(text):
+    # convert the text into lower case
+    text = text.lower()
+
+    # separate the words and make the list
+    text = nltk.word_tokenize(text)
+
+    # remove the special character
+    y = []
+    for i in text:
+        if i.isalnum():
+            y.append(i)
+
+    # remove the stop words and punctuation
+    text = y[:]
+    y.clear()
+    for i in text:
+        if i not in stopwords.words('english') and i not in string.punctuation:
+            y.append(i)
+
+    # perform stemming like loving,loved,love will be converted to love
+    text = y[:]
+    y.clear()
+    for i in text:
+        y.append(ps.stem(i))
+
+    # return the text as a string
+    return " ".join(y)
+
+
+st.title("Email/SMS Classifier")
+
+input_sms = st.text_area("Enter the Email: ")
+
+# Data PreProcessing
+
+if st.button("Predict"):
+    transformed_sms = transform_text(input_sms)
+
+    # vectorize
+    vector_sms = tfidf.transform([transformed_sms]).toarray()
+
+    # predict
+    result = model.predict(vector_sms)
+    if result == 1:
+        st.header("The Email is a Spam.")
+    else:
+        st.header("The Email is not a spam.")
